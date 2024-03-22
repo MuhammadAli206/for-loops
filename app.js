1566be79f00405b92af38d636e30f43666e70b31
MuhammadAli206 <154820260+MuhammadAli206@users.noreply.github.com>
2024-03-22 07:29:55 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+//answer 1
+
+for (let i = 1; i <= 10; i++) {
+     console.log(i);
+    
+}
+//answer 2
+
+for (let i = 2; i <= 20; i += 2) {
+    console.log(i);
+}
+
+//answer 3
+
+for (let i = 1; i <= 15; i += 2) {
+    console.log(i);
+}
+
+//answer 4
+
+// let number = 5;
+// let factorial = 1;
+
+// for (let i = 1; i <= number; i++) {
+//     factorial *= i;
+//     console.log("Factorial of", number, "is:", factorial);
+// }
+
+//answer 5
+
+let tableNumber = prompt("enter a table number")
+for (let i = 1; i<=10; i++) {
+  document.write(tableNumber + 'x' + i + '=' + tableNumber * i +'<br>')
+    
+}
+
+
+
